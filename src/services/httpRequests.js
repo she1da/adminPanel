c0106d5d28de6e890c1cd64134e9d3ca743c7af3
Sheyda <sheydaahmadipour@gmail.com>
2025-05-17 10:36:09 +0330

    httpRequst added to handle api calls

---
+++ src/services/httpRequests.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+
+const BASE_URL = 'http://fullstackweb.ir/api/v2/'
+
+export const httpRequest = axios.create({
+    baseURL:BASE_URL
+})
+
+export const httpInterceptedServices = axios.create({
+    baseURL:BASE_URL
+})
+const isOnline = ()=>{
+    return navigator.onLine
+}
+httpInterceptedServices.interceptors.request.use(
+
+    async (config)=>{
+        const token = localStorage.getItem('token');
+        
+        if(token){
+            // @ts-ignore
+            config.headers = {Authorization:'Bearer ' + token }
+        }
+        return config
+    }
+    , error => Promise.reject(error)
+)
+
+httpInterceptedServices.interceptors.response.use(
+
+   response=>response,
+   async error => { 
+   if( error.response?.status === 500 || error.response?.status === 401 || !isOnline() || error.code === 'ERR_NETWORK'  ){
+    if( !isOnline() || error.code === 'ERR_NETWORK'){
+        alert('unstable network')
+    }
+    if(error.response?.status === 401 ){
+
+        window.location.href = '/login'
+
+
+    }
+   }
+   return Promise.reject(error)
+   }
+)
